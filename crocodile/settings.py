fbccab2c468b4e1094cbd0a2f51cdbadae461df5
Rooster <rooster.hamidi81@gmail.com>
2024-08-08 14:35:52 +0330

    new UI

--- crocodile/settings.py
+++ crocodile/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'crocodile.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../crocodile/crocodile/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
